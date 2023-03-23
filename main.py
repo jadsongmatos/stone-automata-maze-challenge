f73325669ea4bc73327f508cf76cfc44ffb07688
Jadson G. Matos <50336793+jadsongmatos@users.noreply.github.com>
2023-03-23 20:13:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import numpy as np
 import heapq
 
+GERACOES = 250
 # Create a binary matrix of 65x85
-matriz_3d = np.zeros((65, 85,500), dtype=bool)
+matriz_3d = np.zeros((65, 85,GERACOES), dtype=bool)
 
 # Read the file and fill the binary matrix
 with open('input.txt', 'r') as file:
@@ -10,8 +11,6 @@ with open('input.txt', 'r') as file:
         row = list(map(int, line.strip().split()))
         for j, value in enumerate(row):
             matriz_3d[i, j,0] = bool(value)
-
-print(matriz_3d)
 
 def contar_celulas_adjacentes(matriz, i, j):
     altura, largura = matriz.shape
@@ -43,63 +42,119 @@ def proxima_geracao(matriz):
 
     return nova_geracao
 
-geracoes = 498
-index = 0
+for i in range(GERACOES-2):
+    matriz_3d[:, :, i + 1] = proxima_geracao(matriz_3d[:, :, i])
 
-for _ in range(geracoes):
-    matriz_3d[:, :, index + 1] = proxima_geracao(matriz_3d[:, :, index])
+""" 
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+# Define the update function for the animation
+def update(frame):
+    im.set_array(matriz_3d[:, :, frame])
+    return [im]
 
+# Set up the plot
+fig, ax = plt.subplots()
+im = ax.imshow(matriz_3d[:, :, 0], cmap='Greens', animated=True)
+ax.set_title("Cellular Automaton")
 
+# Create the animation
+ani = FuncAnimation(fig, update, frames=GERACOES, blit=True, interval=100)
+
+# Display the animation
+plt.show()
+"""
 print("environment")
 
 def heuristic(a, b):
-    return np.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2 + (b[2] - a[2]) ** 2)
+    return abs(a[0] - b[0]) + abs(a[1] - b[1]) + abs(a[2] - b[2])
 
+def neighbors(matrix, current):
+    x, y, z = current
+    for dx, dy in ((0, 1), (1, 0), (0, -1), (-1, 0)):
+        nx, ny, nz = x + dx, y + dy, z + 1
+        if 0 <= nx < matrix.shape[0] and 0 <= ny < matrix.shape[1] and 0 <= nz < matrix.shape[2] and not matrix[nx, ny, nz]:
+            yield (nx, ny, nz)
 
-def get_neighbors(environment, node):
-    neighbors = []
-    for dx, dy, dz in [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]:
-        x, y, z = node[0] + dx, node[1] + dy, node[2] + dz
-        if 0 <= x < environment.shape[0] and 0 <= y < environment.shape[1] and 0 <= z < environment.shape[2]:
-            if not environment[x, y, z]:
-                neighbors.append((x, y, z))
-    return neighbors
+def a_star(matrix, start):
+    frontier = []
+    heapq.heappush(frontier, (0, start))
+    came_from = dict()
+    cost_so_far = dict()
+    came_from[start] = None
+    cost_so_far[start] = 0
 
+    while frontier:
+        _, current = heapq.heappop(frontier)
+        if current[:2] == (64, 84):
+            break
 
-def find_path(environment):
-    start = (0, 0, 0)
-    goal = (65, 85, environment.shape[2] - 1)
+        for next_node in neighbors(matrix, current):
+            new_cost = cost_so_far[current] + 1
+            if next_node not in cost_so_far or new_cost < cost_so_far[next_node]:
+                cost_so_far[next_node] = new_cost
+                priority = new_cost + heuristic((64, 84, current[2] + 1), next_node)
+                heapq.heappush(frontier, (priority, next_node))
+                came_from[next_node] = current
 
-    if environment[start] or environment[goal]:
-        return None  # No path if start or goal is an obstacle
+    if current[:2] != (64, 84):
+        return None
 
-    open_set = []
-    heapq.heappush(open_set, (0, start))
-    came_from = {}
-    g_score = {start: 0}
-    f_score = {start: heuristic(start, goal)}
+    path = [current]
+    while current != start:
+        current = came_from[current]
+        path.append(current)
+    path.reverse()
+    return path
 
-    while open_set:
-        _, current = heapq.heappop(open_set)
+def path_to_directions(path):
 
-        if current == goal:
-            path = []
-            while current in came_from:
-                path.append(current)
-                current = came_from[current]
-            path.append(start)
-            path.reverse()
-            return path
+    directions = [''] * len(path)
+    for i in range(len(path) - 1):
+        dx, dy = path[i + 1][0] - path[i][0], path[i + 1][1] - path[i][1]
+        if dx == 1:
+            directions[i] = 'R'
+        elif dx == -1:
+            directions[i] = 'L'
+        elif dy == 1:
+            directions[i] = 'U'
+        elif dy == -1:
+            directions[i] = 'D'
+    return directions
 
-        for neighbor in get_neighbors(environment, current):
-            tentative_g_score = g_score[current] + heuristic(current, neighbor)
-            if neighbor not in g_score or tentative_g_score < g_score[neighbor]:
-                came_from[neighbor] = current
-                g_score[neighbor] = tentative_g_score
-                f_score[neighbor] = tentative_g_score + heuristic(neighbor, goal)
-                if neighbor not in [i[1] for i in open_set]:
-                    heapq.heappush(open_set, (f_score[neighbor], neighbor))
+start = (0, 0, 0)
+path = a_star(matriz_3d, start)
 
-    return None  # No path found
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.colors as colors
 
-print(find_path(matriz_3d))
+def update_frame(frame, path, matrix):
+    ax.clear()
+    z = path[frame][2]
+    layer = matrix[:, :, z].copy()
+    cmap_layer = np.zeros_like(layer, dtype=np.int)
+    cmap_layer[layer] = 1  # blocked cells are black
+    for x, y, _ in path[:frame + 1]:
+        cmap_layer[x, y] = 2  # path cells are green
+    ax.imshow(cmap_layer, cmap=custom_cmap)
+    ax.set_title(f"Step {frame}, z = {z}")
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+
+custom_cmap = colors.ListedColormap(["white","green", (0,0,0)])
+
+if path:
+    print("Path found:")
+    print(path)
+    directions = path_to_directions(path)
+    print("Directions:")
+    print(' '.join(str(elem) for elem in directions))
+
+    fig, ax = plt.subplots()
+    ani = animation.FuncAnimation(fig, update_frame, frames=len(path), fargs=(path, matriz_3d), interval=100)
+    plt.show()
+else:
+    print("No path found")
